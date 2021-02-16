355c0ef152108b7030d1e400eb329aab06c45066
Ventseslava Shopova <supervenca@yandex.ru>
2021-02-16 13:54:45 +0200

    timer deadline set

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() => openModal('.modal',modalTimerId), 50000);
+    let deadline = new Date(Date.parse(new Date()) + 5 * 24 * 60 * 60 * 1000);
 
     tabs('.tabheader__item','.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]','.modal',modalTimerId);
-    timer('.timer','2021-03-15');
+    timer('.timer',deadline);
     cards();
     calc();
     slider({
